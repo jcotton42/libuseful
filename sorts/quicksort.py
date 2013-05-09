862e692b0fa3af59a22e9376627a6bf1f9094127
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-09 10:01:36 -0400

    Add doctest

--- sorts/quicksort.py
+++ sorts/quicksort.py
@@ -2,6 +2,10 @@ from random import choice
 def quickSort(a):
 	"""
 	Perform a quicksort on a given list. Return the sorted list.
+	Example:
+
+	>>> quickSort([1, 5, 7, 2, 3, 4, 1])
+	[1, 1, 2, 3, 4, 5, 7]
 	"""
 	if a == []: return [] # Don't infinitely recurse
 	pivot = choice(a) # Choose a pivot value
@@ -9,3 +13,6 @@ def quickSort(a):
 	equal = [i for i in a if i == pivot] # Values equal to the pivot
 	greater = [i for i in a if i > pivot] # And greater than the pivot
 	return quickSort(less) + equal + quickSort(greater) # Recursively sort
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
