be1f4a8642e66d435dabb105376218936bac4ef8
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-09 16:00:48 -0400

    Initial commit of pygrep

---
+++ re/main.py
@@ -0,0 +1,21 @@
+"""
+Pygrep
+Fox Wilson, Joshua Cotton
+5th Period
+Computer Science Final Project (2013)
+"""
+from Tkinter import *
+import re
+class Window(Tk):
+	"""
+	Helper class to make the creation of windows and dialogs easier.
+	"""
+	def __init__(self):
+		self.init()
+		self.mainloop()
+	def init(self):
+		"""
+		Stub method which subclasses use to initialize GUI 
+		components.
+		"""
+		pass
