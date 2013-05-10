840130520a64ddf4a74ec0dfcf4b57bb35c7b113
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-10 17:40:59 -0400

    Fix epydoc choking on test

--- display/test.py
+++ display/test.py
@@ -1,11 +1,14 @@
 from progress import *
 from time import sleep
 from random import randint
-p = ProgressBar(100, twidth = 100)
-while True:
-	for i in range(101):
-		p.update(i)
-		sleep(0.05)
-	for i in range(100, 0, -1):
-		p.update(i)
-		sleep(0.05)
+def test():
+	p = ProgressBar(100, twidth = 10)
+	while True:
+		for i in range(101):
+			p.update(i)
+			sleep(0.05)
+		for i in range(100, 0, -1):
+			p.update(i)
+			sleep(0.05)
+if __name__ == "__main__":
+	test()
