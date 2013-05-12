4c123e7a61ae652ea48f844bb2a7cf70dc1b9468
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 12:13:59 -0400

    doc conf

--- builddocs.sh
+++ builddocs.sh
@@ -7,6 +7,7 @@ PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
 export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -o workspace/docs/source workspace
+cp workspace/docconf.py workspace/docs/source/conf.py
 sphinx-build -b html workspace/docs/source workspace/docs/
 [ -x workspace/lint ] || mkdir workspace/lint
 pylint --persistent=y -f html workspace > workspace/lint/index.html
