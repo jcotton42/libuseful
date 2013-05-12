cef74cc06c7a3e3a4b0b7f4b80017b707ae681d0
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 09:01:42 -0400

    sphinx

--- builddocs.sh
+++ builddocs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 [ -x docs ] || mkdir docs
+[ -x docs/source] || mkdir docs/source
 [ -x docs/html ] || mkdir docs/html
-[ -x docs/pdf ] || mkdir docs/pdf
-epydoc --html . -o docs/html -n libuseful 
-epydoc --pdf . -o docs/pdf -n libuseful
-
+sphinx-apidoc -F -f -o docs/source .
+sphinx-build -b html docs/source docs/html
