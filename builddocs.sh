4f446e96f876e814f3b12341297b291d82a3e185
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 09:23:55 -0400

    why won't it work.

--- builddocs.sh
+++ builddocs.sh
@@ -3,7 +3,8 @@
 [ -x docs/source ] || mkdir docs/source
 [ -x docs/html ] || mkdir docs/html
 cd ..
-python -c 'import sys; sys.path.append("/var/lib/jenkins/home/jobs/libuseful")'
+PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
+export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o docs/source workspace
 sphinx-build -b html workspace/docs/source workspace/docs/html
