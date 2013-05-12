4830747f0d81839d259b7d641cddef4daf115c37
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 09:18:12 -0400

    ... builddocs

--- builddocs.sh
+++ builddocs.sh
@@ -3,5 +3,6 @@
 [ -x docs/source ] || mkdir docs/source
 [ -x docs/html ] || mkdir docs/html
 cd ..
+python -c 'import sys; sys.addsitedir("/var/lib/jenkins/home/jobs/libuseful")'
 sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o docs/source workspace
 sphinx-build -b html workspace/docs/source workspace/docs/html
