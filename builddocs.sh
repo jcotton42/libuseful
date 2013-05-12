d0d22aa56efed51a7a84a6382d0a0215a500dff8
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 09:03:39 -0400

    sphinx

--- builddocs.sh
+++ builddocs.sh
@@ -2,5 +2,5 @@
 [ -x docs ] || mkdir docs
 [ -x docs/source] || mkdir docs/source
 [ -x docs/html ] || mkdir docs/html
-sphinx-apidoc -F -f -o docs/source .
+sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o docs/source .
 sphinx-build -b html docs/source docs/html
