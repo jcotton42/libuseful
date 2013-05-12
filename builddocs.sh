d4d72870e8dbe868838a1dd17764fb8d274c2606
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 11:34:09 -0400

    builddocs automatic

--- builddocs.sh
+++ builddocs.sh
@@ -2,10 +2,9 @@
 rm -r docs
 [ -x docs ] || mkdir docs
 [ -x docs/source ] || mkdir docs/source
-[ -x docs/html ] || mkdir docs/html
 cd ..
 PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
 export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o workspace/docs/source workspace
-sphinx-build -b html workspace/docs/source workspace/docs/html
+sphinx-build -b html workspace/docs/source workspace/docs/
