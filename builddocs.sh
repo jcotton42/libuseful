97a68a6cd8bf9230a0f843875c0a7444a32e7d35
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 12:09:23 -0400

    rm copyright notice?

--- builddocs.sh
+++ builddocs.sh
@@ -6,7 +6,7 @@ cd ..
 PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
 export PYTHONPATH
 python -c 'import sys; print(sys.path)'
-sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o workspace/docs/source workspace
+sphinx-apidoc -F -f -H libuseful -o workspace/docs/source workspace
 sphinx-build -b html workspace/docs/source workspace/docs/
 [ -x workspace/lint ] || mkdir workspace/lint
 pylint --persistent=y -f html workspace > workspace/lint/index.html
