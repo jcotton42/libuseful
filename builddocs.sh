ccc63291a2f8d01291f4738259622e5e91583815
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 12:34:00 -0400

    Debugging builddocs

--- builddocs.sh
+++ builddocs.sh
@@ -7,7 +7,10 @@ PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
 export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -o workspace/docs/source workspace
+echo Copying docconf
 cp workspace/docconf.py workspace/docs/source/conf.py
+cat workspace/docs/source/conf.py
+echo Done
 sphinx-build -b html workspace/docs/source workspace/docs/
 [ -x workspace/lint ] || mkdir workspace/lint
 pylint --persistent=y -f html workspace > workspace/lint/index.html
