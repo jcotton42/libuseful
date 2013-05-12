b8d74baa81ffc64f1a996cea11ea4ce9e4fb12d5
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 11:44:51 -0400

    lint

--- builddocs.sh
+++ builddocs.sh
@@ -8,3 +8,5 @@ export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o workspace/docs/source workspace
 sphinx-build -b html workspace/docs/source workspace/docs/
+[ -x workspace/lint ] || mkdir workspace/lint
+pylint -f html workspace > workspace/lint/index.html
