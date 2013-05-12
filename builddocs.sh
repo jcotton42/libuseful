f83ed13a7c3f6780a69ba35ea035f6b5801922bb
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-12 09:10:24 -0400

    builddocs

--- builddocs.sh
+++ builddocs.sh
@@ -2,5 +2,7 @@
 [ -x docs ] || mkdir docs
 [ -x docs/source] || mkdir docs/source
 [ -x docs/html ] || mkdir docs/html
+PYTHONPATH="${PYTHONPATH}:."
+export PYTHONPATH
 sphinx-apidoc -F -f -H libuseful -A "Fox Wilson, Joshua Cotton" -o docs/source .
 sphinx-build -b html docs/source docs/html
