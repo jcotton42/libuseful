09ced132672a7c49797edef00f2b97ceb766c8af
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-22 11:31:16 -0400

    TODO

--- sorts/__init__.py
+++ sorts/__init__.py
@@ -1,3 +1,7 @@
 """
 This package implements various methods to sort data.
 """
+# TODO: Implement merge sort
+# TODO: Implement heap sort
+# TODO: Implement bogo sort
+# TODO: Implement optimized bogo sort
