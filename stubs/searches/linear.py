5547482b5f9a9fbeb6602f820c1e00e57f9c2af5
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-23 20:39:09 -0400

    stubs

---
+++ stubs/searches/linear.py
@@ -0,0 +1,16 @@
+"""
+This module implements a simple linear search.
+"""
+TARGET_NOT_FOUND = 1
+def linear_search(arr, target, key=lambda x:x):
+    """
+    Perform a linear search on a given list with the given
+    target. Returns TARGET_NOT_FOUND if the target was not
+    found, or the the position of the target if it was
+    found in the form of a tuple (position, object).
+    Example:
+
+    >>> linear_search([5, 2, 3, 1], 3)[0]
+    2
+    """
+    pass
