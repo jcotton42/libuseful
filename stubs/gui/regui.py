7afa39a0346b5e6640034eee706eb2d88c16e0ef
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-23 21:31:55 -0400

    stubs

---
+++ stubs/gui/regui.py
@@ -0,0 +1,22 @@
+"""
+Regexs GUI
+"""
+class REGui(Window):
+    """
+    RE Gui
+    """
+    def initgui(self):
+        """
+        Init gui
+        """
+        pass
+    def loadfile(self):
+        """
+        Load file into interface
+        """
+        pass
+    def search(self):
+        """
+        Perform the search
+        """
+        pass
---
+++ stubs/gui/window.py
@@ -0,0 +1,13 @@
+"""
+Abstracts a GUI window
+"""
+from Tkinter import *
+class Window(Tk):
+    def __init__(self):
+        self.initgui()
+    def initgui():
+        """
+        Defined in subclasses
+        """
+        pass
+
