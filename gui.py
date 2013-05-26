0677faa4ccb33212f1e30c548182d4f5b204ea77
Joshua Cotton <cottons4space@gmail.com>
2013-05-26 15:20:12 -0400

    Structural changes to /gui.py and grammer correctinos in comments

--- gui.py
+++ gui.py
@@ -1,24 +1,13 @@
 from Tkinter import *
 import os, sys
 
-class Window(Tk):
+class MainGui(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
-        self.initgui()
-        self.mainloop()
-    def initgui():
-        """
-        Implemented in subclass
-        """
-        raise NotImplementedError("implement initgui in the subclass please")
 
-class maingui(Window):
-    def __init__(self):
-        Window.__init__(self)
-
-    def initgui(self):
         old_dir = os.getcwd()
         os.chdir(os.path.dirname(__file__)) # Change to root package directory needed for __import__
+        # Add buttons
         for name in os.listdir('.'):
             button_name = ''
             if os.path.isdir(name) and os.path.isfile(name + '/__init__.py'): # if this directory is a package
@@ -32,7 +21,10 @@ class maingui(Window):
                     if '_gui_name' in mod.__dict__: button_name = mod._gui_name
                     else: button_name = name
                     Button(master = self, text = button_name, command = mod.main).pack()
-        os.chdir(old_dir)   # Change to where were
+
+        os.chdir(old_dir)   # Change to where we were
+
+        self.mainloop()
 
 def main():
-    maingui()
+    MainGui()
--- sorts/gui.py
+++ sorts/gui.py
@@ -1,11 +1,8 @@
-from __future__ import absolute_import
-from ..gui import Window
+from Tkinter import *
 
-class sortsgui(Window):
+class SortsGui(Tk):
     def __init__(self):
         pass
-    def initgui(self):
-
 
 def main():
-    pass
+    SortsGui()
