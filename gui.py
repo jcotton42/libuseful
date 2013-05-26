690f6494bb5e2791ecdc5be8ba1c19d4098591d5
Joshua Cotton <cottons4space@gmail.com>
2013-05-26 15:06:07 -0400

    Adding comments to /gui.py and shortened Button statement

--- gui.py
+++ gui.py
@@ -17,16 +17,22 @@ class maingui(Window):
         Window.__init__(self)
 
     def initgui(self):
-        os.chdir(os.path.dirname(__file__))
+        old_dir = os.getcwd()
+        os.chdir(os.path.dirname(__file__)) # Change to root package directory needed for __import__
         for name in os.listdir('.'):
             button_name = ''
-            if os.path.isdir(name) and os.path.isfile(name + '/__init__.py'):
-                mod = __import__(name, globals(), locals(), [], 0)
+            if os.path.isdir(name) and os.path.isfile(name + '/__init__.py'): # if this directory is a package
+                mod = __import__(           # this means import name as mod
+                    name,                   # Name of the module
+                    globals(), locals(),    # Context for the module (all the globlas and locals it can use)
+                    [],                     # the from part
+                    0                       # 0 = absalute import only
+                )
                 if '_gui' in mod.__dict__ and mod._gui == True and 'main' in mod.__dict__:
                     if '_gui_name' in mod.__dict__: button_name = mod._gui_name
                     else: button_name = name
-                    btn = Button(master = self, text = button_name, command = mod.main)
-                    btn.pack()
+                    Button(master = self, text = button_name, command = mod.main).pack()
+        os.chdir(old_dir)   # Change to where were
 
 def main():
     maingui()
