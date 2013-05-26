7587cd9f65ade05f4e32e1598010c3a878f01a85
Joshua Cotton <cottons4space@gmail.com>
2013-05-26 15:17:00 -0400

    Moved searches gui code and added main

--- sorts/gui.py
+++ sorts/gui.py
@@ -1,2 +1,11 @@
+from __future__ import absolute_import
+from ..gui import Window
+
+class sortsgui(Window):
+    def __init__(self):
+        pass
+    def initgui(self):
+
+
 def main():
     pass
