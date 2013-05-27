f53ae9d4465b3cf2275850fa350383c3b3bc47f0
Joshua Cotton <cottons4space@gmail.com>
2013-05-26 23:20:12 -0400

    Continuing re gui

--- plans/re.md
+++ plans/re.md
@@ -5,8 +5,8 @@
 * Input type
     * File
     * Type text
-* Very large textbox for regular expression to run
 * Very large textbox for typing or textbox + Browse... button for file, provides content to run the RE on
+* Very large textbox for regular expression to run
 * Very large textbox for matches
 * Search button
 * Help button (pops up regex and macro (feature of the program) references)
--- re/gui.py
+++ re/gui.py
@@ -1,1 +1,34 @@
 from Tkinter import *
+
+class ReGui(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+
+        self.inputType = StringVar(self)
+        self.inputField = Entry(self)
+        self.reEntry = Text(self, width=60, height=10, wrap=NONE)
+        self.reMatches = Entry(self)
+
+        Label(self, text='Input type: ').grid(row=0, column=0, sticky=W)
+        OptionMenu(self, self.inputType, 'File').grid(row=0, column=1, sticky=W)
+
+        Label(self, text='Input file: ').grid(row=1, column=0, sticky=W)
+        self.inputField.grid(row=1, column=1, columnspan=2, sticky=W, ipadx=60)
+        Button(self, text='Browse...').grid(row=1, column=3, sticky=W)
+
+        Label(self, text='Regular expression:').grid(row=2, column=0, columnspan=4, sticky=W)
+        self.reEntry.grid(row=3, column=0, columnspan=4, sticky=W)
+
+        self.scrollY = Scrollbar(self, orient=VERTICAL, command=self.reEntry.yview)
+        self.scrollY.grid(row=3, column=4, sticky=N+S+W)
+
+        self.scrollX = Scrollbar(self, orient=HORIZONTAL, command=self.reEntry.xview)
+        self.scrollX.grid(row=4, column=0, columnspan=4, sticky=N+E+W)
+
+        self.reEntry.config(yscrollcommand = self.scrollY.set)
+        self.reEntry.config(xscrollcommand = self.scrollX.set)
+
+        self.mainloop()
+
+def main():
+    ReGui()
