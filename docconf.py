8a016e1e19b07a7214ef329b6ff0b86187db61be
Fox Wilson <2016fwilson@tjhsst.edu>
2013-05-28 13:09:08 -0400

    exclude __main__

--- docconf.py
+++ docconf.py
@@ -66,7 +66,7 @@ release = '0.1 alpha'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '*__main__*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
