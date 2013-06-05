5b323d2a802d09fb8bd8bffa72585f058c03494f
Fox Wilson <2016fwilson@tjhsst.edu>
2013-06-05 09:19:39 -0400

    re-add pylint

--- builddocs.sh
+++ builddocs.sh
@@ -2,12 +2,12 @@
 rm -r docs
 [ -x docs ] || mkdir docs
 [ -x docs/source ] || mkdir docs/source
+[ -x pylint ] || mkdir pylint
 cd ..
 PYTHONPATH="${PYTHONPATH}:/var/lib/jenkins/home/jobs/libuseful/"
 export PYTHONPATH
 python -c 'import sys; print(sys.path)'
 sphinx-apidoc -F -f -H libuseful -o workspace/docs/source workspace
-echo Copying docconf
 cp workspace/docconf.py workspace/docs/source/conf.py
-echo Done
 sphinx-build -b html workspace/docs/source workspace/docs/
+pylint -f html workspace > workspace/pylint/index.html
